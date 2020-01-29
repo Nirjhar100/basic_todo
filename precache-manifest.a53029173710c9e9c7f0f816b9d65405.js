f09cc807005c23ac621f13af3415bd448cfed040
Nirjhar Debnath <nirjhar@Nirjhars-MacBook-Air.local>
2020-01-29 18:27:04 +0530

    Updates

---
+++ precache-manifest.a53029173710c9e9c7f0f816b9d65405.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6ba1d1f907a9963df2126d1d0f4f32a",
+    "url": "/basic_todo/index.html"
+  },
+  {
+    "revision": "0bca0bf368b34ad3c155",
+    "url": "/basic_todo/static/css/main.0cd8bd71.chunk.css"
+  },
+  {
+    "revision": "e1a5ebcdc5c024b07498",
+    "url": "/basic_todo/static/js/2.ad3b01b9.chunk.js"
+  },
+  {
+    "revision": "0bca0bf368b34ad3c155",
+    "url": "/basic_todo/static/js/main.ebaa27b5.chunk.js"
+  },
+  {
+    "revision": "e2ea6a210c9c1d1d9c89",
+    "url": "/basic_todo/static/js/runtime-main.99e5313c.js"
+  }
+]);
